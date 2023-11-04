f7319820dc2c0933ee3174ab1dfd28d90b9d442a
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-11-04 05:45:57 +0100

    completed testimonial section

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -20,9 +20,9 @@ export default function TestimonialSection() {
         <div className="bg-[#D9D9D9] rounded-full mx-auto">
           <img src={TestimonialImage} alt="" />
         </div>
-        <div className="my">
-            <img src={TestimonialLine} alt="" />
-
+        <div className="my-24">
+            <img src={TestimonialLine} className="mx-auto mb-48" alt="" />
+            <p className="font-semibold text-3xl text-center">" Choose a job you love, and you’ll never have to work a day in your life "</p>
         </div>
       </div>
     </div>
