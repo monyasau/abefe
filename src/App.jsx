16aa24961df190b8d97ecfe49ecae7cf2535953c
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-11-04 06:05:03 +0100

    started footer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import TopNav from "./components/TopNav";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import TestimonialSection from "./components/TestimonialSection";
+import FooterSection from "./components/FooterSection";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,14 +14,8 @@ function App() {
       <TopNav />
       <HeroSection />
       <AboutSection />
-<TestimonialSection/>
-
-
-      <div className="bg-[#222222]">
-        <div className="max-w-screen-xl mx-auto  text-white">
-          <div className="py-32 text-center">© Olajide Sodiq 2023</div>
-        </div>
-      </div>
+      <TestimonialSection />
+      <FooterSection />
     </>
   );
 }
---
+++ src/components/FooterSection.jsx
@@ -0,0 +1,9 @@
+export default function FooterSection() {
+  return (
+    <div className="bg-[#222222]">
+      <div className="max-w-screen-xl mx-auto  text-white">
+        <div className="py-32 text-center">© Olajide Sodiq 2023</div>
+      </div>
+    </div>
+  );
+}
