efc5491bb349a3e017a4d4d70bb988a63af9b024
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-11-05 22:08:46 +0100

    almost done with footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,9 +1,38 @@
+{
+  /* <h1 className="text-white text-3xl">Let’s work together.</h1> */
+}
+{
+  /* <div className="py-32 text-center">© Olajide Sodiq 2023</div> */
+}
+
+import vectorImage from "../assets/images/Vector.png";
+
 export default function FooterSection() {
   return (
-    <div className="bg-[#222222]">
-      <div className="max-w-screen-xl mx-auto  text-white">
-        <div className="py-32 text-center">© Olajide Sodiq 2023</div>
+    <>
+      <div className="bg-[#222222] py-32 relative grid grid-cols-2 ">
+        <div
+          className="h-80 w-[100%] ml-[10%] px-[15%] mr-auto bg-no-repeat"
+          style={{ backgroundImage: `url(${vectorImage})` }}
+        >
+          <div className="max-w-screen-xl mx-auto py-24">
+            <h1 className=" text-white font-semibold text-3xl">
+              Let’s work together.
+            </h1>
+            <p className="text-[#D9d9d9] my-6 text-xl">
+              Let’s work together to build something great.
+            </p>
+          </div>
+          <div className="my-24 ">
+            <a href="#" className="text-white font-medium hover:underline text-lg">
+              SEE MY WORKS
+            </a>
+            
+          </div>
+        </div>
+
+        <div className="bg-white">dd</div>
       </div>
-    </div>
+    </>
   );
 }
