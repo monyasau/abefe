cdf270ae189065f617b1d88a9bff315394e23294
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-11-25 12:56:05 +0100

    progressing on mobile display

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import arrowIcon from "../assets/images/black-arrow.png";
 
 export default function HeroSection() {
     return (
-        <section className="max-w-screen-xl mx-auto md:grid text-center grid-cols-2 py-12">
+        <section className="max-w-screen-xl mx-auto md:grid md:text-start text-center grid-cols-2 py-12">
             <div className="md:flex md:flex-col md:justify-center">
                 <h1 className="font-extrabold text-4xl md:max-w-sm">A creative product & music designer.</h1>
 
@@ -13,7 +13,7 @@ export default function HeroSection() {
                 </p>
 
                 <div>
-                    <a href="#" className="font-medium hover:underline text-lg">SEE MY WORKS <img src={arrowIcon} alt="" /></a>
+                    <a href="#" className="font-medium hover:underline text-lg flex justify-center md:justify-normal">SEE MY WORKS <img className="mx-8 hidden md:block" src={arrowIcon} alt="" /></a>
                 </div>
             </div>
 
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,122 +1,117 @@
+import { useState } from "react";
+
 export default function TopNav() {
   let dropdownIcon = document.getElementById("dropdownMenuToggleIcon");
   let backdrop = document.getElementById("backdrop");
   let defaultMenu = document.getElementById("defaultNav");
   let smNavBar = document.getElementById("smallScreenNavBar");
-  let smNavCloseBtn = document.getElementById("closeSmNav")
+  let smNavCloseBtn = document.getElementById("closeSmNav");
 
+  const [menu, setMenu] = useState(false);
 
   return (
     <>
-      <nav id="defaultNav" className="hidden md:block border-b">
-        <div className="md:max-w-screen-xl mx-auto py-4 flex w-full justify-between">
-          <div>
-            <a href="/" className="text-xl font-bold">
-              ThisIsAbefe
-            </a>
-          </div>
-
-          <div className="flex gap-14">
-            <a href="#" className="font-medium hover:underline">
-              Home
-            </a>
-            <a href="#" className="text-dark-gray hover:underline">
-              Designs
-            </a>
-            <a href="#" className="text-dark-gray hover:underline">
-              Music
-            </a>
-            <a href="#" className="text-dark-gray hover:underline">
-              Contact
-            </a>
-          </div>
+      <div className="flex justify-between py-2 px-4">
+        <div>
+          <a href="/" className="text-xl font-bold">
+            ThisIsAbefe
+          </a>
         </div>
-      </nav>
 
-
-
-
-
-
-
-
-
-
-<div className="flex justify-between">
-      <div>
-            <a href="/" className="text-xl font-bold">
-              ThisIsAbefe
-            </a>
-          </div>
-      <div  id="dropdownMenuToggleIcon" className=" dropdown align-right md:hidden items-center flex" onClick={() => {
-    backdrop.classList.remove("hidden");
-    smNavBar.classList.remove("hidden")
-    defaultMenu.classList.add("hidden");
-  }}>
-        <a href="#" className="text-slate-600 font-bold mx-4">Menu</a>
-
-
-      <svg width="24" height="24" className="md:hidden">
-          <path
-            d="M12 6v.01M12 12v.01M12 18v.01M12 7a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
-            stroke="currentColor"
-            strokeWidth="1.5"
-          ></path>
-        </svg>
+        <div className="gap-14 hidden sm:flex">
+          <a href="#" className="font-medium hover:underline">
+            Home
+          </a>
+          <a href="#" className="text-dark-gray hover:underline">
+            Designs
+          </a>
+          <a href="#" className="text-dark-gray hover:underline">
+            Music
+          </a>
+          <a href="#" className="text-dark-gray hover:underline">
+            Contact
+          </a>
         </div>
-      
-      
-      <div
-        id="smallScreenNavBar"
-        className="hidden fixed z-50 top-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold text-slate-900 dark:bg-slate-800 dark:text-slate-400 dark:highlight-white/5"
-      >
+
         <div
-          id="closeSmNav"
-          className="absolute top-5 right-5 w-8 h-8 flex justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+          id="dropdownMenuToggleIcon"
+          className="dropdown align-right sm:hidden items-center flex"
+          onClick={() => setMenu(true)}
         >
-          <svg viewBox="0 0 10 10" className="w-2.5 h-2.5 overflow-visible">
+          <a href="#" className="text-slate-600 font-bold mx-4">
+            Menu
+          </a>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
             <path
-              d="M0 0L10 10M10 0L0 10"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
               strokeLinecap="round"
-            ></path>
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
           </svg>
         </div>
-        <ul className="space-y-6">
-          <li>
-            <a className="hover:text-sky-500" href="/">
-              Home
-            </a>
-          </li>
 
-          <li>
-            <a href="/designs" className="hover:text-sky-500">
-              Designs
-            </a>
-          </li>
+        {menu && (
+          <div
+            id="smallScreenNavBar"
+            className="fixed z-50 top-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold text-slate-900 dark:bg-slate-800 dark:text-slate-400 dark:highlight-white/5"
+          >
+            <div
+              id="closeSmNav"
+              onClick={() => setMenu(false)}
+              className="absolute top-5 right-5 w-8 h-8 flex justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+            >
+              <svg viewBox="0 0 10 10" className="w-2.5 h-2.5 overflow-visible">
+                <path
+                  d="M0 0L10 10M10 0L0 10"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </div>
 
-          <li>
-            <a className="hover:text-sky-500" href="/music">
-              Music
-            </a>
-          </li>
+            <ul className="space-y-6">
+              <li>
+                <a className="hover:text-sky-500" href="/">
+                  Home
+                </a>
+              </li>
 
-          <li>
-            <a className="hover:text-red-500" href="/Contact">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
+              <li>
+                <a href="/designs" className="hover:text-sky-500">
+                  Designs
+                </a>
+              </li>
+
+              <li>
+                <a className="hover:text-sky-500" href="/music">
+                  Music
+                </a>
+              </li>
+
+              <li>
+                <a className="hover:text-red-500" href="/Contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
 
       <div
         id="backdrop"
         className="hidden fixed inset-0 z-40 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80"
       ></div>
-      
     </>
   );
 }
